2e4ed2f9262055ded96fbd680eed0190ce0b80b6
Hanno Zwikstra <hanno@hannozwikstra.nl>
2020-08-23 14:15:30 +0200

    Implemented Advanced search (#2)
    * Implemented advanced search
    * added unit tests for all advanced search scenario's; added documentation from Lexicala documentation page to some methods/properties

--- source/Lexicala.Client/LexicalaClient.cs
+++ source/Lexicala.Client/LexicalaClient.cs
@@ -1,16 +1,18 @@
-﻿using Lexicala.NET.Client.Request;
+﻿using System;
+using Lexicala.NET.Client.Request;
 using Lexicala.NET.Client.Response;
 using Lexicala.NET.Client.Response.Entries;
 using Lexicala.NET.Client.Response.Entries.JsonConverters;
+using Lexicala.NET.Client.Response.Languages;
+using Lexicala.NET.Client.Response.Me;
 using Lexicala.NET.Client.Response.Search;
+using Lexicala.NET.Client.Response.Test;
 using Newtonsoft.Json;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
-using Lexicala.NET.Client.Response.Languages;
-using Lexicala.NET.Client.Response.Me;
-using Lexicala.NET.Client.Response.Test;
 
 namespace Lexicala.NET.Client
 {
@@ -47,38 +49,124 @@ namespace Lexicala.NET.Client
             return JsonConvert.DeserializeObject<LanguagesResponse>(response);
         }
 
-        public async Task<SearchResponse> BasicSearchAsync(string searchText, string sourceLanguage, string etag = null)
+        public Task<SearchResponse> BasicSearchAsync(string searchText, string sourceLanguage, string etag = null)
         {
-            var request = new HttpRequestMessage(HttpMethod.Get, $"{Search}?language={sourceLanguage}&text={searchText}");
+            if (sourceLanguage.Length != 2)
+            {
+                throw new ArgumentException($"Invalid language code provided ({sourceLanguage}), a valid language code is two characters");
+            }
+
+            if (string.IsNullOrEmpty(searchText))
+            {
+                throw new ArgumentException("SearchText cannot be empty");
+            }
+
+            return ExecuteSearch($"{Search}?language={sourceLanguage}&text={searchText}", etag);
+        }
+
+        public Task<SearchResponse> AdvancedSearchAsync(AdvancedSearchRequest searchRequest)
+        {
+            if (searchRequest.Language?.Length != 2)
+            {
+                throw new ArgumentException($"Invalid language code provided ({searchRequest.Language}), a valid language code is two characters");
+            }
+
+            if (string.IsNullOrEmpty(searchRequest.SearchText))
+            {
+                throw new ArgumentException("SearchText cannot be empty");
+            }
+
+            // build the querystring based on provided search request params
+            StringBuilder queryStringBuilder = new StringBuilder($"{Search}?language={searchRequest.Language}&text={searchRequest.SearchText}");
+            queryStringBuilder.Append("&source=" + searchRequest.Source);
+            
+            if (searchRequest.Analyzed)
+            {
+                queryStringBuilder.Append("&analyzed=true");
+            }
+            if (searchRequest.Monosemous)
+            {
+                queryStringBuilder.Append("&monosemous=true");
+            }
+            if (searchRequest.Polysemous)
+            {
+                queryStringBuilder.Append("&polysemous=true");
+            }
+            if (searchRequest.Morph)
+            {
+                queryStringBuilder.Append("&morph=true");
+            }
+            if (!string.IsNullOrEmpty(searchRequest.Pos))
+            {
+                queryStringBuilder.Append("&pos=" + searchRequest.Pos);
+            }
+            if (!string.IsNullOrEmpty(searchRequest.Number))
+            {
+                queryStringBuilder.Append("&number=" + searchRequest.Number);
+            }
+            if (!string.IsNullOrEmpty(searchRequest.Gender))
+            {
+                queryStringBuilder.Append("&gender=" + searchRequest.Gender);
+            }
+            if (!string.IsNullOrEmpty(searchRequest.Subcategorization))
+            {
+                queryStringBuilder.Append("&subcategorization=" + searchRequest.Subcategorization);
+            }
+
+            // pagination - only append if values are other than default values
+            if (searchRequest.Page > 1)
+            {
+                queryStringBuilder.Append("&page=" + searchRequest.Page);
+            }
+            if (searchRequest.PageLength != 10 && searchRequest.PageLength > 0 && searchRequest.PageLength <= 30)
+            {
+                queryStringBuilder.Append("&page-length=" + searchRequest.PageLength);
+            }
+            if (searchRequest.Sample > 0)
+            {
+                queryStringBuilder.Append("&sample=" + searchRequest.Sample);
+            }
+
+            return ExecuteSearch(queryStringBuilder.ToString(), searchRequest.ETag);
+        }
+
+        private async Task<SearchResponse> ExecuteSearch(string querystring, string etag)
+        {
+            using var httpRequest = new HttpRequestMessage(HttpMethod.Get, querystring);
+
+            if (etag != null)
+            {
+                httpRequest.Headers.Add("If-None-Match", etag);
+            }
+
+            using var response = await _httpClient.SendAsync(httpRequest);
+
+            // until we have a better error-handling mechanism we just let the code throw built-in exception if request was not successful
+            response.EnsureSuccessStatusCode();
+
+            string result = await response.Content.ReadAsStringAsync();
+
+            var responseObject = JsonConvert.DeserializeObject<SearchResponse>(result, SearchResponseJsonConverter.Settings);
+            responseObject.Metadata = GetResponseMetadata(response.Headers);
+
+            return responseObject;
+        }
+
+
+        public async Task<Entry> GetEntryAsync(string entryId, string etag = null)
+        {
+            using var request = new HttpRequestMessage(HttpMethod.Get, $"{Entries}/{entryId}");
+
             if (etag != null)
             {
                 request.Headers.Add("If-None-Match", etag);
             }
 
-            var response = await _httpClient.SendAsync(request);
+            using var response = await _httpClient.SendAsync(request);
+
+            // until we have a better error-handling mechanism we just let the code throw built-in exception if request was not successful
+            response.EnsureSuccessStatusCode();
             
-            string result = await response.Content.ReadAsStringAsync();
-
-            var responseObject = JsonConvert.DeserializeObject<SearchResponse>(result, SearchResponseJsonConverter.Settings);
-            responseObject.Metadata = GetResponseMetadata(response.Headers);
-            
-            return responseObject;
-        }
-
-        /// <summary>
-        /// TODO - Implement
-        /// </summary>
-        /// <param name="request"></param>
-        /// <returns></returns>
-        public Task<SearchResponse> AdvancedSearchAsync(AdvancedSearchRequest request)
-        {
-            return Task.FromResult(new SearchResponse());
-        }
-
-        public async Task<Entry> GetEntryAsync(string entryId, string etag = null)
-        {
-            var request = new HttpRequestMessage(HttpMethod.Get, $"{Entries}/{entryId}");
-            var response = await _httpClient.SendAsync(request);
             var result = await response.Content.ReadAsStringAsync();
 
             var responseObject = JsonConvert.DeserializeObject<Entry>(result, EntryResponseJsonConverter.Settings);
--- source/Lexicala.Client/Request/AdvancedSearchRequest.cs
+++ source/Lexicala.Client/Request/AdvancedSearchRequest.cs
@@ -3,18 +3,65 @@
     public class AdvancedSearchRequest
     {
         public string ETag { get; set; }
+        /// <summary>
+        /// Specify which resource to look in. (See <see cref="Sources"/>).
+        /// </summary>
         public string Source { get; set; } = Sources.Global;
+        /// <summary>
+        /// Specify a headword.
+        /// </summary>
         public string SearchText { get; set; }
+        /// <summary>
+        /// Specify which source language to look in.
+        /// </summary>
         public string Language { get; set; }
-        public bool Morph { get; set; }
-        public string Pos { get; set; }
-        public string Number{ get; set; }
-        public string Subcategorization { get; set; }
-        public bool Monosemous { get; set; }
-        public bool Polysemous { get; set; }
-        public int Sample { get; set; }
-        public int Page { get; set; }
-        public int PageLength { get; set; }
+
+        /// <summary>
+        /// Setting this to <c>true</c> looks for inflected forms by applying the stemmer.
+        /// </summary>
         public bool Analyzed { get; set; }
+        /// <summary>
+        ///  Find single sense entries only.
+        /// </summary>
+        public bool Monosemous { get; set; }
+        /// <summary>
+        /// Setting this to <c>true</c> looks for all inflected forms (as well as headwords) contained both in KD data and in the external morphological lists.
+        /// </summary>
+        public bool Morph { get; set; }
+        /// <summary>
+        /// Find multiple sense entries only.
+        /// </summary>
+        public bool Polysemous { get; set; }
+
+        /// <summary>
+        /// Specify grammatical gender ( = masculine, feminine, ...).
+        /// </summary>
+        public string Gender { get; set; }
+        /// <summary>
+        /// Specify grammatical number  ( = singular, plural, ...).
+        /// </summary>
+        public string Number{ get; set; }
+        /// <summary>
+        /// Specify part of speech ( = noun, verb, ...).
+        /// </summary>
+        public string Pos { get; set; }
+        /// <summary>
+        /// Specify subcategorization ( = masculine, feminine, ...).
+        /// </summary>
+        public string Subcategorization { get; set; }
+
+        /// <summary>
+        /// Specify the page number out of available_n_pages, in order to navigate between pages.
+        /// The default value is 1.
+        /// </summary>
+        public int Page { get; set; } = 1;
+        /// <summary>
+        /// Specify how many results appear per page.
+        /// </summary>
+        public int PageLength { get; set; } = 10;
+        /// <summary>
+        /// Specify the number of randomly-sampled results to return.
+        /// </summary>
+        public int Sample { get; set; }
     }
 }
